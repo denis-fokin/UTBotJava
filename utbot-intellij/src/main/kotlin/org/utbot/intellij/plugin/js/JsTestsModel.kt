4f844c14321a5abfed77e131c50cede29ed018f2
rudolf101 <st087506@student.spbu.ru>
2022-07-06 13:07:31 +0300

    Create testSourceRoot as VirtualFile

--- utbot-intellij/src/main/kotlin/org/utbot/intellij/plugin/js/JsTestsModel.kt
+++ utbot-intellij/src/main/kotlin/org/utbot/intellij/plugin/js/JsTestsModel.kt
@@ -3,10 +3,15 @@ package org.utbot.intellij.plugin.js
 import com.intellij.lang.javascript.refactoring.util.JSMemberInfo
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
 
 data class JsTestsModel(
     val project: Project,
     val srcModule: Module,
     val testModule: Module,
     val selectedMethods: Set<JSMemberInfo>?,
-)
+) {
+    var testSourceRoot: VirtualFile? = null
+    // TODO testPackageName
+    // TODO testFramework
+}
