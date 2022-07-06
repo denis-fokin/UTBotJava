8839c56daa97243844039d687a00df866cf311a8
rudolf101 <st087506@student.spbu.ru>
2022-07-06 13:56:44 +0300

    Added fileMethods and focusedMethod into JsTestsModel

--- utbot-intellij/src/main/kotlin/org/utbot/intellij/plugin/js/JsTestsModel.kt
+++ utbot-intellij/src/main/kotlin/org/utbot/intellij/plugin/js/JsTestsModel.kt
@@ -1,5 +1,6 @@
 package org.utbot.intellij.plugin.js
 
+import com.intellij.lang.javascript.psi.JSFunction
 import com.intellij.lang.javascript.refactoring.util.JSMemberInfo
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
@@ -9,7 +10,8 @@ data class JsTestsModel(
     val project: Project,
     val srcModule: Module,
     val testModule: Module,
-    val selectedMethods: Set<JSMemberInfo>?,
+    val fileMethods: Set<JSMemberInfo>?,
+    val focusedMethod: Set<JSFunction>?,
 ) {
     var testSourceRoot: VirtualFile? = null
     // TODO testPackageName
