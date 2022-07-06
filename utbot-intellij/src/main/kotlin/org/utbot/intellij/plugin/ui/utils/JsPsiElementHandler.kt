7a9111a3e0ae7de5417f80f18622e42fbc91441b
zishkaz <loktevsergay.2003@gmail.com>
2022-07-06 10:55:48 +0300

    WIP JsPsiElementHandler.kt and build.gradle change

--- utbot-intellij/src/main/kotlin/org/utbot/intellij/plugin/ui/utils/JsPsiElementHandler.kt
+++ utbot-intellij/src/main/kotlin/org/utbot/intellij/plugin/ui/utils/JsPsiElementHandler.kt
@@ -1,32 +1,53 @@
 package org.utbot.intellij.plugin.ui.utils
 
 
+import com.intellij.codeInsight.TestFrameworks
 import com.intellij.lang.ecmascript6.psi.ES6Class
 import com.intellij.lang.javascript.psi.JSFunction
-import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiAnonymousClass
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiElementFactory
 import com.intellij.psi.util.PsiTreeUtil
-import com.intellij.testIntegration.TestIntegrationUtils
-import com.intellij.testIntegration.createTest.CreateTestAction
+import org.jetbrains.uast.UClass
 import org.jetbrains.uast.toUElement
+import org.jetbrains.uast.toUElementOfType
 
 class JsPsiElementHandler(
     override val classClass: Class<ES6Class> = ES6Class::class.java,
     override val methodClass: Class<JSFunction> = JSFunction::class.java,
-): PsiElementHandler {
-    /**
-     * Makes a transition from Js to UAST and then to Psi.
-     */
+) : PsiElementHandler {
     @Suppress("UNCHECKED_CAST")
     override fun <T> toPsi(element: PsiElement, clazz: Class<T>): T {
-        return element.toUElement()?.javaPsi as? T ?: error("Could not cast $element to $clazz")
+        val fac = PsiElementFactory.getInstance(element.project)
+        when {
+            element is JSFunction -> {
+                return fac.createMethod(element.name!!, null) as? T ?: error("Could not cast $element to $clazz")
+            }
+            element is ES6Class -> {
+                return fac.createClass(element.name!!) as? T ?: error("Could not cast $element to $clazz")
+            }
+            else -> error("Could not cast $element to $clazz")
+        }
     }
 
-    override fun isCreateTestActionAvailable(element: PsiElement): Boolean =
-        CreateTestAction.isAvailableForElement(element)
+    override fun isCreateTestActionAvailable(element: PsiElement): Boolean {
+        containingClass(element)?.let { psiClass ->
+            val psiFile = element.containingFile
+            if (psiFile.containingDirectory == null) return false
+            if (psiClass.isAnnotationType ||
+                psiClass is PsiAnonymousClass
+            ) {
+                return false
+            }
+            return TestFrameworks.detectFramework(psiClass) == null
+        }
+        return false
+
+    }
 
     override fun containingClass(element: PsiElement) =
-        if (PsiTreeUtil.getParentOfType(element, PsiClass::class.java, false) != null) {
-            TestIntegrationUtils.findOuterClass(element)
-        } else null
+        PsiTreeUtil.getParentOfType(element, ES6Class::class.java, false)?.let {
+            val fac = PsiElementFactory.getInstance(element.project)
+            fac.createClass(it.name!!)
+        }
 }
