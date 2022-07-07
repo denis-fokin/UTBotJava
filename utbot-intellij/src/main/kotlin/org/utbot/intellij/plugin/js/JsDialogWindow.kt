399f86b5f9198ed9fa2d5c53cca711bc51378ea9
rudolf101 <st087506@student.spbu.ru>
2022-07-07 14:23:23 +0300

    Add BaseModel support and create framework WIP

--- utbot-intellij/src/main/kotlin/org/utbot/intellij/plugin/js/JsDialogWindow.kt
+++ utbot-intellij/src/main/kotlin/org/utbot/intellij/plugin/js/JsDialogWindow.kt
@@ -2,12 +2,22 @@ package org.utbot.intellij.plugin.js
 
 import com.intellij.lang.javascript.refactoring.ui.JSMemberSelectionTable
 import com.intellij.lang.javascript.refactoring.util.JSMemberInfo
+import com.intellij.openapi.ui.ComboBox
 import com.intellij.openapi.ui.DialogPanel
 import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.ui.ContextHelpLabel
+import com.intellij.ui.components.Panel
+import com.intellij.ui.layout.CellBuilder
+import com.intellij.ui.layout.Row
 import com.intellij.ui.layout.panel
 import com.intellij.util.ui.JBUI
+import org.utbot.framework.codegen.TestFramework
+import org.utbot.framework.plugin.api.CodeGenerationSettingItem
+import org.utbot.intellij.plugin.ui.components.TestFolderComboWithBrowseButton
+import java.awt.BorderLayout
+import javax.swing.DefaultComboBoxModel
 import javax.swing.JComponent
-
+import javax.swing.JPanel
 
 class JsDialogWindow(val model: JsTestsModel): DialogWrapper(model.project) {
 
@@ -20,9 +30,16 @@ class JsDialogWindow(val model: JsTestsModel): DialogWrapper(model.project) {
         }
     }
 
-    private val testSourceFolderField = JsTestFolderComboWithBrowseButton(model)
+    private val testSourceFolderField = TestFolderComboWithBrowseButton(model)
+
+    private val testFrameworks = ComboBox(DefaultComboBoxModel(TestFramework.allItems.toTypedArray()))
 
     private lateinit var panel: DialogPanel
+
+    @Suppress("UNCHECKED_CAST")
+    private val itemsToHelpTooltip = hashMapOf(
+        (testFrameworks as ComboBox<CodeGenerationSettingItem>) to ContextHelpLabel.create(""),
+    )
 
     init {
         title = "Generate tests with UtBot"
@@ -30,17 +47,23 @@ class JsDialogWindow(val model: JsTestsModel): DialogWrapper(model.project) {
         init()
     }
 
+    @Suppress("UNCHECKED_CAST")
     override fun createCenterPanel(): JComponent {
         panel = panel {
             row("Test source root:") {
                 component(testSourceFolderField)
+            }
+            row("Test framework:") {
+                makePanelWithHelpTooltip(
+                    testFrameworks as ComboBox<CodeGenerationSettingItem>,
+                    itemsToHelpTooltip[testFrameworks]
+                )
             }
             row("Generate test methods for:") {}
             row {
                 scrollPane(functionsTable)
             }
         }
-        initDefaultValues()
         checkBoxUpdate()
         return panel
     }
@@ -58,11 +81,14 @@ class JsDialogWindow(val model: JsTestsModel): DialogWrapper(model.project) {
         }
     }
 
-    private fun initDefaultValues() {
-    }
-
-    private fun setListeners() {
-    }
-
     private fun checkMembers(members: Collection<JSMemberInfo>) = members.forEach { it.isChecked = true }
+
+    private fun Row.makePanelWithHelpTooltip(
+        mainComponent: JComponent,
+        contextHelpLabel: ContextHelpLabel?
+    ): CellBuilder<JPanel> =
+        component(Panel().apply {
+            add(mainComponent, BorderLayout.LINE_START)
+            contextHelpLabel?.let { add(it, BorderLayout.LINE_END) }
+        })
 }
