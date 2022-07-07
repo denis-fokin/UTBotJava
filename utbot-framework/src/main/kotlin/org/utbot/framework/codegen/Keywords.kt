b38ec65ac6b318e9c2881013e304d2d727cc2b95
rudolf101 <st087506@student.spbu.ru>
2022-07-07 16:02:54 +0300

    Added jsKeywords

--- utbot-framework/src/main/kotlin/org/utbot/framework/codegen/Keywords.kt
+++ utbot-framework/src/main/kotlin/org/utbot/framework/codegen/Keywords.kt
@@ -16,6 +16,15 @@ private val kotlinHardKeywords = setOf(
     "val", "var", "when", "while"
 )
 
+private val jsKeywords = setOf(
+    "abstract", "arguments", "await", "boolean", "break", "byte", "case", "catch", "char", "class", "const", "continue",
+    "debugger", "default", "delete", "do", "double", "else", "enum", "eval", "export", "extends", "false", "final",
+    "finally", "float", "for", "function", "goto", "if", "implements", "import", "in", "instanceof", "int", "interface",
+    "let", "long", "native", "new", "null", "package", "private", "protected", "public", "return", "short", "static",
+    "super", "switch", "synchronized", "this", "throw", "throws", "transient", "true", "try", "typeof", "var", "void",
+    "volatile", "while", "with", "yield"
+)
+
 @Suppress("unused")
 private val kotlinSoftKeywords = setOf(
     "by", "catch", "constructor", "delegate", "dynamic", "field", "file", "finally", "get", "import", "init",
