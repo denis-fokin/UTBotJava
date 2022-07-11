c724339dc7b22ec30359528ab5a24427932cceec
Andrey Tarbeev <github@tarbeev.su>
2022-07-11 17:10:36 +0300

    Set parametrized test setting

--- utbot-framework/src/test/kotlin/org/utbot/framework/codegen/TestCodeGeneratorPipeline.kt
+++ utbot-framework/src/test/kotlin/org/utbot/framework/codegen/TestCodeGeneratorPipeline.kt
@@ -265,7 +265,7 @@ class TestCodeGeneratorPipeline(private val testFrameworkConfiguration: TestFram
                     mockFramework = MockFramework.defaultItem,
                     mockStrategy = MockStrategyApi.defaultItem,
                     staticsMocking = StaticsMocking.defaultItem,
-                    parametrizedTestSource = ParametrizedTestSource.defaultItem,
+                    parametrizedTestSource = ParametrizedTestSource.PARAMETRIZE,
                     forceStaticMocking = ForceStaticMocking.defaultItem,
                 )
             )
