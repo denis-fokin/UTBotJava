497eb7f3ef8dc210980eefa83b4bae340d8cc750
Vladislav Kasimov <st087506@student.spbu.ru>
2022-07-14 19:40:28 +0300

    JsFuzzer improvement
    * Added support for iterations(do..while, for, while)
    * Added support for switch cases
    * Code cleaning

--- utbot-intellij/src/main/kotlin/org/utbot/intellij/plugin/js/fuzzer/JSFuzz.kt
+++ utbot-intellij/src/main/kotlin/org/utbot/intellij/plugin/js/fuzzer/JSFuzz.kt
@@ -1,107 +1,41 @@
 package org.utbot.intellij.plugin.js.fuzzer
 
-import com.oracle.js.parser.ir.*
-import com.oracle.truffle.api.strings.TruffleString
+import JsAstVisitor
+import com.oracle.js.parser.ir.FunctionNode
 import org.utbot.framework.plugin.api.ClassId
 import org.utbot.framework.plugin.api.MethodId
-import org.utbot.framework.plugin.api.util.*
-import org.utbot.fuzzer.*
+import org.utbot.fuzzer.FuzzedMethodDescription
+import org.utbot.fuzzer.FuzzedValue
+import org.utbot.fuzzer.ModelProvider
+import org.utbot.fuzzer.fuzz
 import org.utbot.intellij.plugin.js.fuzzer.providers.JsConstantsModelProvider
 import org.utbot.intellij.plugin.js.fuzzer.providers.JsStringModelProvider
 import org.utbot.intellij.plugin.js.fuzzer.providers.JsUndefinedModelProvider
-
-fun getTreeConst(
-    tree: Expression?,
-    lastFuzzedOp: FuzzedOp = FuzzedOp.NONE,
-    fuzzedConcreteValues: MutableSet<FuzzedConcreteValue> = mutableSetOf()
-): Set<FuzzedConcreteValue> {
-
-    val compOp = """>=|<=|>|<|==|!=""".toRegex()
-    val curOp = compOp.find(tree.toString())?.value
-    val fuzzedOp = FuzzedOp.values().find { curOp == it.sign } ?: FuzzedOp.NONE
-    when (tree) {
-        is JoinPredecessorExpression -> {
-            getTreeConst((tree.expression as? BinaryNode)?.lhs, fuzzedOp, fuzzedConcreteValues)
-            getTreeConst(
-                (tree.expression as? BinaryNode)?.rhs,
-                fuzzedOp.reverseOrElse { FuzzedOp.NONE },
-                fuzzedConcreteValues
-            )
-        }
-        is LiteralNode<*> -> {
-            when (tree.value) {
-                is TruffleString -> {
-                    fuzzedConcreteValues.add(
-                        FuzzedConcreteValue(
-                            ClassId("<string>"),
-                            tree.value.toString(),
-                            lastFuzzedOp
-                        )
-                    )
-                }
-                is Boolean -> {
-                    fuzzedConcreteValues.add(FuzzedConcreteValue(booleanClassId, tree.value, lastFuzzedOp))
-                }
-                is Integer -> {
-                    fuzzedConcreteValues.add(FuzzedConcreteValue(intClassId, tree.value, lastFuzzedOp))
-                }
-                is Long -> {
-                    fuzzedConcreteValues.add(FuzzedConcreteValue(longClassId, tree.value, lastFuzzedOp))
-                }
-                is Double -> {
-                    fuzzedConcreteValues.add(FuzzedConcreteValue(doubleClassId, tree.value, lastFuzzedOp))
-                }
-            }
-        }
-        is BinaryNode -> {
-            getTreeConst((tree as? BinaryNode)?.lhs, fuzzedOp, fuzzedConcreteValues)
-            getTreeConst((tree as? BinaryNode)?.rhs, fuzzedOp.reverseOrElse { FuzzedOp.NONE }, fuzzedConcreteValues)
-        }
-    }
-    return fuzzedConcreteValues
-}
-
-fun getIfNodes(node: Node?, IfNodes: MutableList<IfNode> = mutableListOf()): List<IfNode> {
-    when (node) {
-        is IfNode -> {
-            node.fail?.let { getIfNodes(node.fail.statements.first() as? IfNode, IfNodes) }
-            node.pass?.let { getIfNodes(node.pass.statements.first() as? IfNode, IfNodes) }
-            IfNodes.add(node)
-        }
-    }
-    return IfNodes
-}
-
 
 class JsClassId(private val jsName: String) : ClassId(jsName) {
     override val simpleName: String
         get() = jsName
 }
 
-
-fun jsFuzzing(modelProvider: (ModelProvider) -> ModelProvider = { it }, method: FunctionNode): Sequence<List<FuzzedValue>> {
+fun jsFuzzing(
+    modelProvider: (ModelProvider) -> ModelProvider = { it },
+    method: FunctionNode
+): Sequence<List<FuzzedValue>> {
     val execId = MethodId(
         JsClassId("debug"),
         method.name.toString(),
         jsUndefinedClassId,
         method.parameters.toList().map { jsUndefinedClassId }
     )
-
-    val allStatements = mutableListOf<IfNode>()
-    method.body.statements.forEach { statement ->
-        allStatements.addAll(getIfNodes(statement))
-    }
-    val allConstants = mutableSetOf<FuzzedConcreteValue>()
-    allStatements.forEach {
-        getTreeConst(it.test, FuzzedOp.NONE, allConstants)
-    }
-
-    val modelProviderWithFallback = modelProvider(ModelProvider.of(
-        JsConstantsModelProvider,
-        JsUndefinedModelProvider,
-        JsStringModelProvider,
-    ))
-    val methodUnderTestDescription = FuzzedMethodDescription(execId, allConstants).apply {
+    method.body.accept(JsAstVisitor)
+    val modelProviderWithFallback = modelProvider(
+        ModelProvider.of(
+            JsConstantsModelProvider,
+            JsUndefinedModelProvider,
+            JsStringModelProvider,
+        )
+    )
+    val methodUnderTestDescription = FuzzedMethodDescription(execId, JsAstVisitor.fuzzedConcreteValues).apply {
         compilableName = method.name.toString()
         val names = method.parameters.map { it.name.toString() }
         parameterNameMap = { index -> names.getOrNull(index) }
---
+++ utbot-intellij/src/main/kotlin/org/utbot/intellij/plugin/js/fuzzer/JsAstVisitor.kt
@@ -0,0 +1,65 @@
+import com.oracle.js.parser.ir.BinaryNode
+import com.oracle.js.parser.ir.LexicalContext
+import com.oracle.js.parser.ir.LiteralNode
+import com.oracle.js.parser.ir.Node
+import com.oracle.js.parser.ir.visitor.NodeVisitor
+import com.oracle.truffle.api.strings.TruffleString
+import org.utbot.framework.plugin.api.util.booleanClassId
+import org.utbot.fuzzer.FuzzedConcreteValue
+import org.utbot.fuzzer.FuzzedOp
+import org.utbot.intellij.plugin.js.fuzzer.jsDoubleClassId
+import org.utbot.intellij.plugin.js.fuzzer.jsIntClassId
+import org.utbot.intellij.plugin.js.fuzzer.jsLongClassId
+import org.utbot.intellij.plugin.js.fuzzer.jsStringClassId
+
+object JsAstVisitor : NodeVisitor<LexicalContext>(LexicalContext()) {
+    var lastFuzzedOpGlobal = FuzzedOp.NONE
+    val fuzzedConcreteValues = mutableSetOf<FuzzedConcreteValue>()
+
+    override fun enterBinaryNode(binaryNode: BinaryNode?): Boolean {
+        binaryNode?.let {
+            val compOp = """>=|<=|>|<|==|!=""".toRegex()
+            val curOp = compOp.find(binaryNode.toString())?.value
+            val currentFuzzedOp = FuzzedOp.values().find { curOp == it.sign } ?: FuzzedOp.NONE
+            lastFuzzedOpGlobal = currentFuzzedOp
+            validateNode(it.lhs)
+            lastFuzzedOpGlobal = lastFuzzedOpGlobal.reverseOrElse { FuzzedOp.NONE }
+            validateNode(it.rhs)
+        }
+
+        return false
+    }
+
+    private fun validateNode(literalNode: Node) {
+        if (literalNode !is LiteralNode<*>) return
+        when (literalNode.value) {
+            is TruffleString -> {
+                fuzzedConcreteValues.add(
+                    FuzzedConcreteValue(
+                        jsStringClassId,
+                        literalNode.value.toString(),
+                        lastFuzzedOpGlobal
+                    )
+                )
+            }
+            is Boolean -> {
+                fuzzedConcreteValues.add(
+                    FuzzedConcreteValue(
+                        booleanClassId,
+                        literalNode.value,
+                        lastFuzzedOpGlobal
+                    )
+                )
+            }
+            is Integer -> {
+                fuzzedConcreteValues.add(FuzzedConcreteValue(jsIntClassId, literalNode.value, lastFuzzedOpGlobal))
+            }
+            is Long -> {
+                fuzzedConcreteValues.add(FuzzedConcreteValue(jsLongClassId, literalNode.value, lastFuzzedOpGlobal))
+            }
+            is Double -> {
+                fuzzedConcreteValues.add(FuzzedConcreteValue(jsDoubleClassId, literalNode.value, lastFuzzedOpGlobal))
+            }
+        }
+    }
+}
